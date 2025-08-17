2cb301fd7efa75eb3ff4baaf43f3dcd9aa66b2f7
naka123 <naka123@kukabarra.info>
2025-08-17 10:30:02 +0600

    break/disable "account" in app

--- app/src/mobile/menu/index.ts
+++ app/src/mobile/menu/index.ts
@@ -12,7 +12,7 @@ import {openCard} from "../../card/openCard";
 import {activeBlur} from "../util/keyboardToolbar";
 import {initAI} from "../settings/ai";
 import {initRiffCard} from "../settings/riffCard";
-import {login, showAccountInfo} from "../settings/account";
+// import {login, showAccountInfo} from "../settings/account";
 import {openModel} from "./model";
 import {initAbout} from "../settings/about";
 import {getRecentDocs} from "./getRecentDocs";
@@ -38,17 +38,17 @@ export const popMenu = () => {
 
 export const initRightMenu = (app: App) => {
     const menuElement = document.getElementById("menu");
-    let accountHTML = "";
-    if (window.siyuan.user && !window.siyuan.config.readonly) {
-        accountHTML = `<div class="b3-menu__item" id="menuAccount">
-    <img class="b3-menu__icon" src="${window.siyuan.user.userAvatarURL}"/>
-    <span class="b3-menu__label">${window.siyuan.user.userName}</span>
-</div>`;
-    } else if (!window.siyuan.config.readonly) {
-        accountHTML = `<div class="b3-menu__item" id="menuAccount">
-    <svg class="b3-menu__icon"><use xlink:href="#iconAccount"></use></svg><span class="b3-menu__label">${window.siyuan.languages.login}</span>
-</div>`;
-    }
+//    let accountHTML = "";
+//     if (window.siyuan.user && !window.siyuan.config.readonly) {
+//         accountHTML = `<div class="b3-menu__item" id="menuAccount">
+//     <img class="b3-menu__icon" src="${window.siyuan.user.userAvatarURL}"/>
+//     <span class="b3-menu__label">${window.siyuan.user.userName}</span>
+// </div>`;
+//     } else if (!window.siyuan.config.readonly) {
+//         accountHTML = `<div class="b3-menu__item" id="menuAccount">
+//     <svg class="b3-menu__icon"><use xlink:href="#iconAccount"></use></svg><span class="b3-menu__label">${window.siyuan.languages.login}</span>
+// </div>`;
+//     }
 
     let aiHTML = `<div class="b3-menu__item${window.siyuan.config.readonly ? " fn__none" : ""}" id="menuAI">
         <svg class="b3-menu__icon"><use xlink:href="#iconSparkles"></use></svg><span class="b3-menu__label">AI</span>
@@ -64,7 +64,6 @@ export const initRightMenu = (app: App) => {
     <span class="b3-menu__label">${window.siyuan.languages.back}</span>
 </div>
 <div class="b3-menu__items">
-    ${accountHTML}
     <div id="menuRecent" class="b3-menu__item">
         <svg class="b3-menu__icon"><use xlink:href="#iconList"></use></svg><span class="b3-menu__label">${window.siyuan.languages.recentDocs}</span>
     </div>
@@ -293,15 +292,15 @@ export const initRightMenu = (app: App) => {
                 event.preventDefault();
                 event.stopPropagation();
                 break;
-            } else if (target.id === "menuAccount") {
-                event.preventDefault();
-                event.stopPropagation();
-                if (document.querySelector("#menuAccount img")) {
-                    showAccountInfo();
-                    return;
-                }
-                login();
-                break;
+            // } else if (target.id === "menuAccount") {
+            //     event.preventDefault();
+            //     event.stopPropagation();
+            //     if (document.querySelector("#menuAccount img")) {
+            //         showAccountInfo();
+            //         return;
+            //     }
+            //     login();
+            //     break;
             }
             target = target.parentElement;
         }
