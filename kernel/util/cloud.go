cfc44c0c815a4890299cd72a9b33cba456343494
naka123 <naka123@kukabarra.info>
2025-08-17 10:28:56 +0600

    break/disable siyuan cloud

--- kernel/util/cloud.go
+++ kernel/util/cloud.go
@@ -65,20 +65,20 @@ func GetCloudForumAssetsServer() string {
 }
 
 const (
-	chinaServer            = "https://siyuan-sync.b3logfile.com"    // 中国大陆云端服务地址，阿里云负载均衡，用于接口（数据同步文件上传、下载会走七牛云 OSS ChinaSyncServer）
-	chinaWebSocketServer   = "wss://siyuan-sync.b3logfile.com"      // 中国大陆云端服务地址，阿里云负载均衡，用于接口（数据同步文件上传、下载会走七牛云 OSS ChinaSyncServer）
-	chinaSyncServer        = "https://siyuan-data.b3logfile.com/"   // 中国大陆云端数据同步服务地址，七牛云 OSS，用于数据同步文件上传、下载
-	chinaCloudAssetsServer = "https://assets.b3logfile.com/siyuan/" // 中国大陆云端图床服务地址，用于导出预览模式下订阅会员渲染图床
-	chinaAccountServer     = "https://ld246.com"                    // 中国大陆链滴服务地址，用于账号登录、分享发布帖子
-	chinaForumAssetsServer = "https://b3logfile.com/file/"          // 中国大陆链滴图床服务地址，用于发布文章到社区
+	chinaServer            = "1https://siyuan-sync.b3logfile.com"    // 中国大陆云端服务地址，阿里云负载均衡，用于接口（数据同步文件上传、下载会走七牛云 OSS ChinaSyncServer）
+	chinaWebSocketServer   = "1wss://siyuan-sync.b3logfile.com"      // 中国大陆云端服务地址，阿里云负载均衡，用于接口（数据同步文件上传、下载会走七牛云 OSS ChinaSyncServer）
+	chinaSyncServer        = "1https://siyuan-data.b3logfile.com/"   // 中国大陆云端数据同步服务地址，七牛云 OSS，用于数据同步文件上传、下载
+	chinaCloudAssetsServer = "1https://assets.b3logfile.com/siyuan/" // 中国大陆云端图床服务地址，用于导出预览模式下订阅会员渲染图床
+	chinaAccountServer     = "1https://ld246.com"                    // 中国大陆链滴服务地址，用于账号登录、分享发布帖子
+	chinaForumAssetsServer = "1https://b3logfile.com/file/"          // 中国大陆链滴图床服务地址，用于发布文章到社区
 
-	northAmericaServer            = "https://siyuan-cloud.liuyun.io"   // 北美云端服务地址，Cloudflare
-	northAmericaWebSocketServer   = "wss://siyuan-cloud.liuyun.io"     // 北美云端服务地址，Cloudflare，用于接口，数据同步文件上传、下载会走七牛云 OSS ChinaSyncServer
-	northAmericaSyncServer        = "https://siyuan-data.liuyun.io/"   // 北美云端数据同步服务地址，七牛云 OSS，用于数据同步文件上传、下载
-	northAmericaCloudAssetsServer = "https://assets.liuyun.io/siyuan/" // 北美云端图床服务地址，用于导出预览模式下订阅会员渲染图床
-	northAmericaAccountServer     = "https://liuyun.io"                // 流云服务地址，用于账号登录、分享发布帖子
-	northAmericaForumAssetsServer = "https://assets.liuyun.io/file/"   // 北美云端图床服务地址，用于发布文章到社区
+	northAmericaServer            = "1https://siyuan-cloud.liuyun.io"   // 北美云端服务地址，Cloudflare
+	northAmericaWebSocketServer   = "1wss://siyuan-cloud.liuyun.io"     // 北美云端服务地址，Cloudflare，用于接口，数据同步文件上传、下载会走七牛云 OSS ChinaSyncServer
+	northAmericaSyncServer        = "1https://siyuan-data.liuyun.io/"   // 北美云端数据同步服务地址，七牛云 OSS，用于数据同步文件上传、下载
+	northAmericaCloudAssetsServer = "1https://assets.liuyun.io/siyuan/" // 北美云端图床服务地址，用于导出预览模式下订阅会员渲染图床
+	northAmericaAccountServer     = "1https://liuyun.io"                // 流云服务地址，用于账号登录、分享发布帖子
+	northAmericaForumAssetsServer = "1https://assets.liuyun.io/file/"   // 北美云端图床服务地址，用于发布文章到社区
 
-	BazaarStatServer = "https://bazaar.b3logfile.com" // 集市包统计服务地址，七牛云，全球 CDN
-	BazaarOSSServer  = "https://oss.b3logfile.com"    // 云端对象存储地址，七牛云，仅用于读取集市包，全球 CDN
+	BazaarStatServer = "1https://bazaar.b3logfile.com" // 集市包统计服务地址，七牛云，全球 CDN
+	BazaarOSSServer  = "https://oss.b3logfile.com"     // 云端对象存储地址，七牛云，仅用于读取集市包，全球 CDN
 )
