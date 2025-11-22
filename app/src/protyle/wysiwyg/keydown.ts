13e24c61314d91861ce7639e4589d25e27a70d6b
naka123 <naka123@kukabarra.info>
2025-11-22 23:37:23 +0600

    toggleAIGeneratedAttribute - hotkey

--- app/src/protyle/wysiwyg/keydown.ts
+++ app/src/protyle/wysiwyg/keydown.ts
@@ -48,7 +48,7 @@ import {
 import {fontEvent} from "../toolbar/Font";
 import {addSubList, listIndent, listOutdent} from "./list";
 import {newFileContentBySelect, rename, replaceFileName} from "../../editor/rename";
-import {cancelSB, insertEmptyBlock, insertTimestampedBlock, jumpToParent} from "../../block/util";
+import {cancelSB, insertEmptyBlock, insertTimestampedBlock, toggleAIGeneratedAttribute, jumpToParent} from "../../block/util";
 import {isLocalPath} from "../../util/pathName";
 /// #if !MOBILE
 import {openBy, openFileById} from "../../editor/util";
@@ -1876,6 +1876,13 @@ export const keydown = (protyle: IProtyle, editorElement: HTMLElement) => {
             event.preventDefault();
             event.stopPropagation();
             insertTimestampedBlock(protyle, nodeElement, range);
+            return;
+        }
+
+        if (!event.repeat && matchHotKey(window.siyuan.config.keymap.editor.general.toggleAIGenerated.custom, event)) {
+            event.preventDefault();
+            event.stopPropagation();
+            toggleAIGeneratedAttribute(protyle, nodeElement);
             return;
         }
 
