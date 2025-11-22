d580c79d4e97d3e757dbcec07ff00acce56c4a2f
naka123 <naka123@kukabarra.info>
2025-11-22 06:21:05 +0600

    PDF asset - непонятно, работает оно или нет

--- kernel/util/path.go
+++ kernel/util/path.go
@@ -302,6 +302,7 @@ var (
 	SiYuanAssetsImage = []string{".apng", ".ico", ".cur", ".jpg", ".jpe", ".jpeg", ".jfif", ".pjp", ".pjpeg", ".png", ".gif", ".webp", ".bmp", ".svg", ".avif"}
 	SiYuanAssetsAudio = []string{".mp3", ".wav", ".ogg", ".m4a", ".flac"}
 	SiYuanAssetsVideo = []string{".mov", ".weba", ".mkv", ".mp4", ".webm"}
+	SiYuanAssetsOther = []string{".pdf"}
 )
 
 // IsPossiblyImage 模糊判断指定文件链接是否可能是图片。
@@ -347,6 +348,9 @@ func IsDisplayableAsset(p string) bool {
 		return true
 	}
 	if gulu.Str.Contains(ext, SiYuanAssetsVideo) {
+		return true
+	}
+	if gulu.Str.Contains(ext, SiYuanAssetsOther) {
 		return true
 	}
 	return false
