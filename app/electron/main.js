d6b751ab12ae2a4e8b3827757871f9a1c2f9ed6c
naka123 <naka123@kukabarra.info>
2025-12-02 22:39:41 +0600

    cleartype "fix" hack

--- app/electron/main.js
+++ app/electron/main.js
@@ -653,6 +653,9 @@ const initKernel = (workspace, port, lang) => {
 
 app.setAsDefaultProtocolClient("siyuan");
 
+app.commandLine.appendSwitch("enable-lcd-text");
+app.commandLine.appendSwitch("disable-prefer-compositing-to-lcd-text");
+app.commandLine.appendSwitch("disable-gpu-driver-bug-workarounds");
 app.commandLine.appendSwitch("disable-web-security");
 app.commandLine.appendSwitch("auto-detect", "false");
 app.commandLine.appendSwitch("no-proxy-server");
