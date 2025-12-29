0f78a58441e93b864e181606d45dd3a07d8e0e76
naka123 <naka123@kukabarra.info>
2025-12-30 01:13:12 +0600

    tag hints

--- app/src/protyle/wysiwyg/index.ts
+++ app/src/protyle/wysiwyg/index.ts
@@ -2307,7 +2307,7 @@ export class WYSIWYG {
             if (!blockElement) {
                 return;
             }
-            if ([":", "(", "【", "（", "[", "{", "「", "『", "#", "/", "、"].includes(event.data)) {
+            if ([":", "(", "【", "（", "[", "{", "\\", "、"].includes(event.data)) {
                 protyle.hint.enableExtend = true;
             }
             if (event.isComposing || isComposition ||
