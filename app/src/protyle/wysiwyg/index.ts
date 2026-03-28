60ad0fce32109e9c9b5059517e701d451b593d9e
naka123 <naka123@kukabarra.info>
2026-03-29 03:26:29 +0600

    image width -0.5%

--- app/src/protyle/wysiwyg/index.ts
+++ app/src/protyle/wysiwyg/index.ts
@@ -2493,7 +2493,7 @@ export class WYSIWYG {
             if (!blockElement) {
                 return;
             }
-            if ([":", "(", "【", "（", "[", "{", "「", "『", "#", "/", "、"].includes(event.data)) {
+            if ([":", "(", "【", "（", "[", "{", "\\", "、"].includes(event.data)) {
                 protyle.hint.enableExtend = true;
             }
             if (event.isComposing || isComposition ||
