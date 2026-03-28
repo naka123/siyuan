fee24872f1ea4b85b9b00f99411d6253f7165fd6
naka123 <naka123@kukabarra.info>
2026-03-29 03:26:29 +0600

    image width -0.5%

--- app/src/protyle/wysiwyg/index.ts
+++ app/src/protyle/wysiwyg/index.ts
@@ -2333,7 +2333,7 @@ export class WYSIWYG {
             if (!blockElement) {
                 return;
             }
-            if ([":", "(", "【", "（", "[", "{", "「", "『", "#", "/", "、"].includes(event.data)) {
+            if ([":", "(", "【", "（", "[", "{", "\\", "、"].includes(event.data)) {
                 protyle.hint.enableExtend = true;
             }
             if (event.isComposing || isComposition ||
