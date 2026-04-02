a24993e8f45cb878b2ef6eb7330d4db18e28fa97
naka123 <naka123@kukabarra.info>
2026-04-02 23:53:27 +0600

    getDocRefLinks in/out

--- kernel/api/router.go
+++ kernel/api/router.go
@@ -231,6 +231,7 @@ func ServeAPI(ginServer *gin.Engine) {
 	ginServer.Handle("POST", "/api/ref/getBacklink2", model.CheckAuth, getBacklink2)
 	ginServer.Handle("POST", "/api/ref/getBacklinkDoc", model.CheckAuth, getBacklinkDoc)
 	ginServer.Handle("POST", "/api/ref/getBackmentionDoc", model.CheckAuth, getBackmentionDoc)
+	ginServer.Handle("POST", "/api/ref/getDocRefLinks", model.CheckAuth, getDocRefLinks)
 
 	ginServer.Handle("POST", "/api/attr/getBookmarkLabels", model.CheckAuth, getBookmarkLabels)
 	ginServer.Handle("POST", "/api/attr/resetBlockAttrs", model.CheckAuth, model.CheckAdminRole, model.CheckReadonly, resetBlockAttrs)
