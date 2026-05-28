2a89183b48e12a097f5596a10f65031a4f0f03d1
naka123 <naka123@kukabarra.info>
2026-05-28 17:23:28 +0600

    hide "Go Home" / "Go End"

--- app/src/protyle/scroll/index.ts
+++ app/src/protyle/scroll/index.ts
@@ -3,7 +3,7 @@ import {onGet} from "../util/onGet";
 import {fetchPost} from "../../util/fetch";
 import {updateHotkeyTip} from "../util/compatibility";
 import {hasClosestByClassName} from "../util/hasClosest";
-import {goEnd, goHome} from "../wysiwyg/commonHotkey";
+// import {goEnd, goHome} from "../wysiwyg/commonHotkey";
 import {showTooltip} from "../../dialog/tooltip";
 
 export class Scroll {
@@ -16,15 +16,15 @@ export class Scroll {
     constructor(protyle: IProtyle) {
         this.parentElement = document.createElement("div");
         this.parentElement.classList.add("protyle-scroll");
-        this.parentElement.innerHTML = `<div class="protyle-scroll__up ariaLabel" data-position="north" aria-label="${updateHotkeyTip("⌘Home")}">
+        this.parentElement.innerHTML = `<!--<div class="protyle-scroll__up ariaLabel" data-position="north" aria-label="${updateHotkeyTip("⌘Home")}">
     <svg><use xlink:href="#iconUp"></use></svg>
-</div>
+</div>-->
 <div class="fn__none protyle-scroll__bar ariaLabel" data-position="2west" aria-label="Blocks 1/1">
     <input class="b3-slider" type="range" max="1" min="1" step="1" value="1" />
 </div>
-<div class="protyle-scroll__down ariaLabel" aria-label="${updateHotkeyTip("⌘End")}">
+<!--<div class="protyle-scroll__down ariaLabel" aria-label="${updateHotkeyTip("⌘End")}">
     <svg><use xlink:href="#iconDown"></use></svg>
-</div>`;
+</div>-->`;
 
         this.element = this.parentElement.querySelector(".protyle-scroll__bar");
         this.keepLazyLoad = false;
@@ -47,11 +47,12 @@ export class Scroll {
         /// #endif
         this.parentElement.addEventListener("click", (event) => {
             const target = event.target as HTMLElement;
-            if (hasClosestByClassName(target, "protyle-scroll__up")) {
-                goHome(protyle);
-            } else if (hasClosestByClassName(target, "protyle-scroll__down")) {
-                goEnd(protyle);
-            } else if (target.classList.contains("b3-slider")) {
+            // if (hasClosestByClassName(target, "protyle-scroll__up")) {
+            //     goHome(protyle);
+            // } else if (hasClosestByClassName(target, "protyle-scroll__down")) {
+            //     goEnd(protyle);
+            // } else
+            if (target.classList.contains("b3-slider")) {
                 this.setIndex(protyle);
             }
         });
