fbd3b105334ec49a1adde3d3232111a03ffecf0a
naka123 <naka123@kukabarra.info>
2026-05-31 09:18:17 +0600

    Back out "app - moveOutlineHeading -> move"
    Original commit changeset: 8ab378e476e3

--- app/src/layout/dock/Outline.ts
+++ app/src/layout/dock/Outline.ts
@@ -455,12 +455,12 @@ export class Outline extends Model {
                         this.element.setAttribute("data-loading", "true");
 
                         transaction(editor, [{
-                            action: "move",
+                            action: "moveOutlineHeading",
                             id: item.dataset.nodeId,
                             previousID,
                             parentID,
                         }], [{
-                            action: "move",
+                            action: "moveOutlineHeading",
                             id: item.dataset.nodeId,
                             previousID: undoPreviousID,
                             parentID: undoParentID,
