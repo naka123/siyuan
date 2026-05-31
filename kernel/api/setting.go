2da3b5133c8b7b0804f4b5b661af7ddec8733cb1
naka123 <naka123@kukabarra.info>
2026-05-31 08:54:31 +0600

    fake getCloudUser

--- kernel/api/setting.go
+++ kernel/api/setting.go
@@ -694,7 +694,7 @@ func getPublish(c *gin.Context) {
 	}
 }
 
-func getCloudUser(c *gin.Context) {
+func getCloudUserOrig(c *gin.Context) {
 	ret := gulu.Ret.NewResult()
 	defer c.JSON(http.StatusOK, ret)
 
@@ -714,6 +714,48 @@ func getCloudUser(c *gin.Context) {
 	}
 	model.RefreshUser(token)
 	ret.Data = model.Conf.GetUser()
+}
+
+func getCloudUser(c *gin.Context) {
+	ret := gulu.Ret.NewResult()
+	defer c.JSON(http.StatusOK, ret)
+
+	if !model.IsAdminRoleContext(c) {
+		return
+	}
+
+	user := &conf.User{
+		UserId:                          "0",
+		UserName:                        "_",
+		UserAvatarURL:                   "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7",
+		UserHomeBImgURL:                 "",
+		UserTitles:                      []*conf.UserTitle{},
+		UserIntro:                       "",
+		UserNickname:                    "",
+		UserCreateTime:                  "29991231 00:00:00",
+		UserSiYuanProExpireTime:         -1,
+		UserToken:                       "token",
+		UserTokenExpireTime:             "32503593600",
+		UserSiYuanRepoSize:              0,
+		UserSiYuanPointExchangeRepoSize: 0,
+		UserSiYuanAssetSize:             0,
+		UserTrafficUpload:               0,
+		UserTrafficDownload:             0,
+		UserTrafficAPIGet:               0,
+		UserTrafficAPIPut:               0,
+		UserTrafficTime:                 0,
+		UserSiYuanSubscriptionPlan:      0,
+		UserSiYuanSubscriptionStatus:    0,
+		UserSiYuanSubscriptionType:      1,
+		UserSiYuanOneTimePayStatus:      1,
+	}
+
+	model.Conf.User = user
+	data, _ := gulu.JSON.MarshalJSON(user)
+	model.Conf.UserData = util.AESEncrypt(string(data))
+	model.Conf.Save()
+
+	ret.Data = user
 }
 
 func logoutCloudUser(c *gin.Context) {
